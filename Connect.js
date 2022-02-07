2f497308cdff45cb95d6fcb8b2f6e6ec538c2d63
xb52 <xb52@cornell.edu>
2022-02-07 04:41:42 -0500

    deploy contract on Munbai TestNet

--- Connect.js
+++ Connect.js
@@ -8,7 +8,7 @@ const connect = new Promise((res, rej) => {
     //res("wallect connected");
 
     let web3 = new Web3(window.ethereum);
-    let contract = new web3.eth.Contract(abi, "0xE9E7a1f955e479a27489eB57B03753001E5FC86f");
+    let contract = new web3.eth.Contract(abi, "0xb316c2c07fa2eab8c6ec7735ad02eb058fac6031");
 
     web3.eth.getAccounts().then((accounts) => {
         console.log("account[0]:" + accounts[0]);
